9b2b9d2f010d2d69a87799a70e700a87a9ee8de4
Anton <anton.lagosha.99@mail.ru>
2019-09-10 00:02:27 +0500

    commented code removed

--- main.cpp
+++ main.cpp
@@ -6,14 +6,8 @@
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-//    MainWindow w;
-//    w.show();
-
-//    compress_file("D:\\USER\\Documents\\text.txt", "D:\\USER\\Documents\\test_huffman.txt");
-//    decompress_file("D:\\USER\\Documents\\test_huffman.txt", "D:\\USER\\Documents\\text_copy.txt");
-
-    compress_file("D:\\USER\\Documents\\main.cpp", "D:\\USER\\Documents\\main_c.cpp");
-    decompress_file("D:\\USER\\Documents\\main_c.cpp", "D:\\USER\\Documents\\main_d.cpp");
+    MainWindow w;
+    w.show();
 
     return a.exec();
 }
