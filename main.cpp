2ef80fbd4d6131523199c856ee841ea1bc1791e1
Anton <anton.lagosha.99@mail.ru>
2019-09-09 14:59:44 +0500

    GUI added

--- main.cpp
+++ main.cpp
@@ -6,11 +6,11 @@
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-//    MainWindow w;
-//    w.show();
+    MainWindow w;
+    w.show();
 
-    compress_file("D:\\USER\\Documents\\text.txt", "D:\\USER\\Documents\\test_huffman.txt");
-    decompress_file("D:\\USER\\Documents\\test_huffman.txt", "D:\\USER\\Documents\\text_copy.txt");
+//    compress_file("D:\\USER\\Documents\\text.txt", "D:\\USER\\Documents\\test_huffman.txt");
+//    decompress_file("D:\\USER\\Documents\\test_huffman.txt", "D:\\USER\\Documents\\text_copy.txt");
 
     return a.exec();
 }
--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,11 +1,42 @@
 #include "mainwindow.hpp"
 #include "ui_mainwindow.h"
 
+#include "huffmanencoding.hpp"
+
+#include <QFileDialog>
+
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    QObject::connect(ui->exitPushButton, &QPushButton::clicked, qApp, &QApplication::exit);
+    QObject::connect(ui->viewPushButton, &QPushButton::clicked, [this] {
+        ui->lineEdit->setText(QFileDialog::getOpenFileName(this, "", "/"));
+    });
+
+    QObject::connect(ui->startPushButton, &QPushButton::clicked, [this]{
+        const QString pathToFile = ui->lineEdit->text();
+        if(pathToFile.isEmpty()) {
+            return;
+        }
+        const std::string pathFrom = pathToFile.toStdString();
+
+        const QString path = QFileDialog::getSaveFileName(this, "", "/");
+        if(path.isEmpty()) {
+            return;
+        }
+
+        const std::string pathTo = path.toStdString();
+
+        if(ui->compressRadioButton->isChecked()) {
+            compress_file(pathFrom, pathTo);
+        }
+        else {
+            decompress_file(pathFrom, pathTo);
+        }
+    });
 }
 
 MainWindow::~MainWindow() { delete ui; }
